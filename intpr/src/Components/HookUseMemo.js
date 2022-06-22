02c2d0fbbf9eec9368c3c1e8208285a015b1da6f
Sanganu <kppkmsanu@gmail.com>
2022-06-22 08:30:29 -0600

    UseMemo completed

--- intpr/src/Components/HookUseMemo.js
+++ intpr/src/Components/HookUseMemo.js
@@ -4,7 +4,8 @@ import {Button} from "react-bootstrap";
 
 const HookUseMemo = ()=>{
     const [empData, setEmpData] = useState()
-    const [change,setChange] = useState("false")
+    const [change,setChange] = useState("false");
+    const [userData,setUserData] = useState([])
     const fetchDataFromApi =()=>{
         fetch("https://jsonplaceholder.typicode.com/users")
         .then(apiResults => {
@@ -17,19 +18,35 @@ const HookUseMemo = ()=>{
             console.error(error)
         })
     }
-//    useEffect(() => {
-     
-//        fetchDataFromApi()
-//         console.log("Executing useFETCH")
-//     }, [])
-   const handleChange = () => {
-    setChange(!change)
+    const handleChange = () => {
+        setChange(!change)
+        fetch("https://randomuser.me/api/?results=5")
+        .then(apiResults => {
+            return apiResults.json()
+        }).then(apiResults => {
+            
+           let userResults =  apiResults.results.map(ele => ({
+                name:`${ele.name.title}.${ele.name.first} ${ele.name.last}`,
+                phone:ele.phone,
+                username:ele.login.username,
+                website:ele.picture.large,
+                email:ele.email,
+                company :{
+                    name:ele.id.name
+                }
+            }))
+            setUserData(userResults)
+            console.log("Button  execution to change userData state.....No console for useMemo")
+             
+        }).catch(error => {
+            console.error(error)
+        })
    }
     
 
     const timeConsuming = useMemo(() =>{
          fetchDataFromApi()
-        console.log("Executing useMEMO")
+        console.log("Executing useMemo hook - state variable empData ")
     } ,[])
 
     return(<div className="container border border-warning text-center">
@@ -47,13 +64,29 @@ const HookUseMemo = ()=>{
                 In this example, we have an expensive function that runs on every render.
                 </p>
 
-            <Button variant="info" onClick={handleChange}>Change boolean state variable...Check your console</Button>
+            <Button variant="info" onClick={handleChange}>Change state variable userData...Check your console to see useMemo execution</Button>
             {/* <Button variant="info" onClick={() => setChange(!change)}>Change boolean state variable...Check your console</Button> */}
             {timeConsuming}
+            <h6>Get API Data - this fetch is called within useMemo</h6>
                 <article className="d-flex flex-wrap">
+               
                         {empData &&
                             empData.map((element, key) => <ApiRecord props={element} key={key} />)}
-                    </article>   
+                    </article>  
+                    <h6>Get API Data - this fetch is called with the button onClick to change state userData and to see the useMemo(empData) is not getting executed.</h6>
+
+                     <article className="d-flex flex-wrap">
+                    
+                        {userData &&
+                            userData.map((ele, key) =>                       
+                           (<ApiRecord 
+                            props={ele} 
+                            key={key}></ApiRecord>)
+                            )
+                            }
+                    </article>  
+
+                     
         
         </div>)
 }
